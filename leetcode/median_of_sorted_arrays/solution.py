afef30e8774f224127429a5d0b554c0cc5957de2
Marcin Wiśniewski <marcinwisniewski707@gmail.com>
2025-02-17 09:38:35 +0100

    refactor of file structure, added leetcode solutions

---
+++ leetcode/median_of_sorted_arrays/solution.py
@@ -0,0 +1,16 @@
+#Leetcode hard
+#https://leetcode.com/problems/median-of-two-sorted-arrays
+
+def findMedianSortedArrays(nums1, nums2):
+    m = len(nums1)
+    n = len(nums2)
+    nums = nums1 + nums2
+    nums.sort()
+    length_of_two_arrays = m + n
+    if length_of_two_arrays % 2 == 0:
+        return (nums[length_of_two_arrays // 2 - 1] + nums[length_of_two_arrays // 2]) / 2
+    return nums[length_of_two_arrays // 2]
+
+
+
+print(findMedianSortedArrays([2, 3],[]))
